5ac135f639b7c01ebe2eef157119f96fc49e6401
Seth M Morton <seth.m.morton@gmail.com>
2012-05-03 21:06:06 -0400

    Added checks that bumpy and scipy are available

--- spectral_exchange.py
+++ spectral_exchange.py
@@ -1,5 +1,5 @@
 from __future__ import print_function, division
-from sys import argv, exit
+from sys import argv, exit, stderr
 
 '''\
 Spectral exchange can be run from the command line non-interactively
@@ -8,6 +8,23 @@ the input, or it can be run as an interactive GUI.
 '''
 
 if __name__ == '__main__':
+
+    # Try to import numpy and scipy, because these are needed
+    # no matter the execution method
+    try:
+        import numpy
+    except ImportError:
+        print('It appears that you are missing numpy.', file=stderr)
+        print('spectral_exchange requires numpy, so you should', file=stderr)
+        print('install it using your favorite method', file=stderr)
+        exit(1)
+    try:
+        import scipy
+    except ImportError:
+        print('It appears that you are missing scipy.', file=stderr)
+        print('spectral_exchange requires scipy, so you should', file=stderr)
+        print('install it using your favorite method', file=stderr)
+        exit(1)
 
     # If an argument was given, then run the non-interactive mode
     if len(argv) >= 1:
