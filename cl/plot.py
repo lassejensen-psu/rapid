7b6a7966ffd1690704dfa5d2108713b42236b289
Seth M Morton <seth.m.morton@gmail.com>
2012-05-03 21:04:49 -0400

    Renamed output.py to plot.py and reworked plot to fit new model

---
+++ cl/plot.py
@@ -0,0 +1,38 @@
+from __future__ import print_function, division
+from sys import stderr
+
+def plot(args, x, y):
+    '''Plots the normalized data.'''
+    try:
+        from matplotlib.pyplot import rc, plot, xlim, ylim, \
+                                      xlabel, ylabel, show
+    except ImportError:
+        print('It appears that you are missing matplotlib.', file=stderr)
+        print('You should install it using your favorite method', file=stderr)
+        return 1
+    from .common import normalize, clip
+    
+    # Normalize the generated data
+    y = normalize(y)
+    # Repeat for the raw data if given.  Clip according to the xlimits
+    if args.raw is not None:
+        args.raw = clip(args.raw, args.xlim)
+        args.raw[:,1] = normalize(args.raw[:,1])
+
+    # 14 point font size
+    rc('font', **{'size': 14})
+
+    # Plot the data and set the data window
+    if args.raw is not None:
+        plot(x, y, 'b-', args.raw[:,0], args.raw[:,1], 'g-', lw=1.5)
+    else:
+        plot(x, y, 'b-', lw=1.5)
+    if args.reverse:
+        xlim(args.xlim[1], args.xlim[0])
+    else:
+        xlim(args.xlim[0], args.xlim[1])
+    ylim(-0.1, 1.2)
+    xlabel('Wavenumbers')
+    ylabel('Intensity (Normalized)')
+    show()
+    return 0
